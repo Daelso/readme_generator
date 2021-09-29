08219bbca867fb4d08f7fee4928741447e4083c1
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-09-29 15:43:40 -0400

    First go around, prints title to readme

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Closely studied https://github.com/jpd61/README-generator for assitance in creating this
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require("util");
+const generateMarkdown = require('./utils/generateMarkdown');
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+function prompts (){
+return inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'projTitle',
+    },
+    {
+      type: 'input',
+      message: 'Write a short description of your project:',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please list installation instructions:',
+      name: 'installInstructions',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage of this project?',
+        name: 'usageInstructions',
+      },
+      {
+        type: 'input',
+        message: 'Please list any contribution guidelines:',
+        name: 'contributionInstructions',
+      },
+    ])}
+
+
+
+// // TODO: Create a function to initialize app
+
+async function init() {
+
+    var answers = await prompts()
+    console.log(answers)
+    const generateREADME = generateMarkdown(answers);
+    await writeFileAsync('./test/README.md', generateREADME);    
+
+}
+
+// // // Function call to initialize app
 init();
